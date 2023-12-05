45ad0ea9813436549d01c4904a927a132fdd8342
Zahi <102051293+Z4HI@users.noreply.github.com>
2023-12-05 02:51:35 -0500

    layered core html, pseudocode javascript

---
+++ scripts.js
@@ -0,0 +1,28 @@
+function app(){
+
+function getUserchoice(){
+//onclick saves user choice
+//rock,paper,scissor img click
+}
+
+function getComputerchoice(){
+//math.random computer choice between 1-3
+}
+
+function outcome(getUserchoice, getComputerchoice){
+ 
+// if userchoice is scissors and comp choice is scissors,
+// outcome = tie, +0
+
+// if user choice is scissors and comp choice is rock
+//outcome = computer +1
+
+// if user choice is scissors and comp choice is paper
+//outcome = user +1
+
+//etc..
+
+//if user counter = 5 you win
+//if computer counter =5 you lose
+}
+}
